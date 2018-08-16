c8574a38fbe41d920807b32b4c7abcbb602debbd
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-08-16 20:05:50 +0600

    Button Added

--- src/Components/AlbumDetail.js
+++ src/Components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) => {
     const {title, artist, thumbnail_image, image} = album;
@@ -25,6 +26,11 @@ const AlbumDetail = ({album}) => {
                 <Image style={styles.imagelStyle}
                        source={{uri: image}}/>
             </CardSection>
+
+            <CardSection>
+                <Button/>
+            </CardSection>
+
         </Card>
     );
 };
